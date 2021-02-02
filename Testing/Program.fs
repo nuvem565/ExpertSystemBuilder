1014b1e183ad668cf11522009e69ef98603ff787
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-02 14:54:47 +0100

    Prints information about default source program
    Default program to interpretetion is DEMO.txt. It's communicated to user. Sets a var for this default text file.

--- Testing/Program.fs
+++ Testing/Program.fs
@@ -20,3 +20,10 @@ let errorMsg = new StringBuilder()
 errorMsg.Append "\r\n --- Warnings and errors section ---\r\n\r\n" |> ignore
 let commandLog = new StringBuilder()
 commandLog.Append "\r\n --- Used commands section ---\r\n\r\n" |> ignore
+
+// Which file to load?
+let demoProgramFile = "DEMO.txt"
+printfn "Demo file is in \"Testing\\Testing\\\" path."
+printfn "Write the file name (with .txt) for parsing or type ENTER for %s " demoProgramFile
+printfn "UTF-8 files allowable only"
+
