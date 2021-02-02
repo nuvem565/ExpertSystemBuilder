d9b5a56785781321214c0b8f0b58a321e302fa36
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-02 14:44:44 +0100

    Set module
    Sets new module for expert system interpreter main program. Includes some  defauld lines.

--- Testing/Program.fs
+++ Testing/Program.fs
@@ -1,3 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+// to open in F# Interactive
+// #r """C:\Users\Tomasz\source\repos\fparsec-master\FParsec\bin\Debug\net45\FParsecCS.dll"""
+// #r """C:\Users\Tomasz\source\repos\fparsec-master\FParsec\bin\Debug\net45\FParsec.dll"""
+
+module TestingES
 ////////// IMPORT OF INPUT TEXT
 
 let errorMsg = new StringBuilder()
