de5366dab08ef87952149d0a901c91ea920b4132
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-02 14:42:25 +0100

    Adds command log builder
    StringBuilder for error logs.

---
+++ Testing/Program.fs
@@ -0,0 +1,6 @@
+////////// IMPORT OF INPUT TEXT
+
+let errorMsg = new StringBuilder()
+errorMsg.Append "\r\n --- Warnings and errors section ---\r\n\r\n" |> ignore
+let commandLog = new StringBuilder()
+commandLog.Append "\r\n --- Used commands section ---\r\n\r\n" |> ignore
