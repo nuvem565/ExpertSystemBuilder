1e8750a94c9f8d7f24b7e8c7a848eef3a30e46dc
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-13 15:24:40 +0100

    Some alias and simple combinations of parsers.
    Alias and some combinations with ws - whitespaces parser.

--- Testing/SimpleParsers.fs
+++ Testing/SimpleParsers.fs
@@ -5,3 +5,13 @@ open System.Collections.Generic
 
 // AUXILIARY PARSER FUNCTIONS
 
+let ws = spaces 
+let ws1 = spaces1
+let ch c = pchar c
+let str s = skipString s
+let strCI s = skipStringCI s
+let str_ws s = skipString s >>. ws
+let strCI_ws s = skipStringCI s >>. ws
+let ws_str s = skipStringCI s .>> ws
+let ws_strCI s = skipStringCI s .>> ws
+
