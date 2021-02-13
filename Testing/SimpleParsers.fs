3606a1dab56698bfff18b2eab2ae4d616d78b80b
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-13 15:38:34 +0100

    Function removes comments from source program in conventional way
    Consists of two local recursive functions representing two states of the program - inside and outside of the comment. Results in the same program string without comments.

--- Testing/SimpleParsers.fs
+++ Testing/SimpleParsers.fs
@@ -19,3 +19,21 @@ let ws_strCI s = skipStringCI s .>> ws
 let comment s = str "//*" >>. skipManyTill ((skipChar '*' >>? skipChar '/') <|> skipNewline) s
 
 
+let removeComments s = 
+    let rec skipTillComment (s:char list) = 
+        match s with
+        | '*' :: '/' :: rest -> rest
+        | '\n' :: rest | '\r' :: rest -> '\n' :: '\r' :: rest
+        | _ :: rest -> skipTillComment rest
+        | [] -> []
+    
+    let rec searchForComments acc (s:char list) : string = 
+        match s with
+        | [] -> 
+            new System.String(Array.ofList acc |> Array.rev) 
+        | '/' :: '*' :: rest -> 
+            searchForComments acc (skipTillComment rest)
+        | ch :: rest -> searchForComments (ch :: acc) rest
+
+    searchForComments [] (Seq.toList s)
+    
