d185182fa1d598163b9cbb4583d3a10d3658db48
nuvem565 <tom.jedrzejewski565@gmail.com>
2021-02-15 18:04:41 +0100

    Calculates truthness for values of qualifier basing on one input value and linear interpolation

--- Testing/SimpleFunctions.fs
+++ Testing/SimpleFunctions.fs
@@ -57,3 +57,23 @@ let fuzzyAND (a:float) b = a * b
 
 let fuzzyNOT (a:float) = 1. - a
 
+let fuzzifyVar (fs: (string * (((float * float) list) option)) list) (x: float) = 
+    // membership function (y) of each value for one single input (x) 
+    let rec mfValue (x:float) (points: (float * float) list) =
+        match points with
+        | p1 :: p2 :: _ when X(p1) <= x && x <= X(p2) -> 
+            Y(p1) + (x - X(p1)) * ( Y(p2) - Y(p1) )/( X(p2) - X(p1) ) // linear interpolation
+        | p :: [] -> Y(p) // singleton (one point fuzzy set) defuzzifier case or exceded max value in set
+        | [] -> 0.0
+        | _ :: rest -> mfValue x rest
+
+    [
+        for value in fs do
+            match value with
+            | valName, Some points -> 
+                match points with
+                | firstPoint :: _ when x < X(firstPoint) -> yield (valName, Y firstPoint) // the value is lower than expected in fuzzy set
+                | _ -> yield (valName, mfValue x points)
+            | valName, _ -> yield (valName, 0.0) 
+    ]
+
